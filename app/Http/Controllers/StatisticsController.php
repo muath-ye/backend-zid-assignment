17e67ed73fb1343e8e455de51156d25acfa0e4b9
muathye <muath.ye@gmail.com>
2022-10-05 23:35:53 +0300

    Add items count statistics

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Item;
+use Illuminate\Http\JsonResponse;
 
 class StatisticsController extends Controller
 {
-    //
+    public function itemsCount(): JsonResponse
+    {
+        return new JsonResponse(['item' => Item::get()->count()]);
+    }
 }
